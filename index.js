dfdd22aa9dfcf735983334935acedba780ee8bab
Sayan Das <sayandas001s@gmail.com>
2023-03-18 19:17:17 +0530

    Restructuring

--- index.js
+++ index.js
@@ -9,34 +9,34 @@
 // @grant        none
 // ==/UserScript==
 
-var video_input;
-var video_canvas;
-var video_ctx;
-var video_player = document.createElement('video');
-var temp_canvas = document.createElement('canvas');
-var temp_canvas_ctx = temp_canvas.getContext('2d')
+let video_input
+let video_canvas
+let video_ctx
+const video_player = document.createElement('video')
+const temp_canvas = document.createElement('canvas')
+const temp_canvas_ctx = temp_canvas.getContext('2d')
 
 video_player.addEventListener("play", () => {
     function step() {
-    video_ctx.drawImage(video_player, 0, 0, video_canvas.width, video_canvas.height); 
-      for (classes of ["yt-core-image--fill-parent-height","style-scope yt-img-shadow"]){
-        let list = document.getElementsByClassName(classes);
-        temp_canvas.width = list[0].width;
-        temp_canvas.height = list[0].height;
+    video_ctx.drawImage(video_player, 0, 0, video_canvas.width, video_canvas.height)
+      for (let classes of ["yt-core-image--fill-parent-height","style-scope yt-img-shadow"]){
+        let list = document.getElementsByClassName(classes)
+        temp_canvas.width = list[0].width
+        temp_canvas.height = list[0].height
         for (let i of list){
-          let pos = i.getBoundingClientRect();
+          let pos = i.getBoundingClientRect()
           if (pos.top-100+pos.height > 0 && pos.top-100 < video_player.height && pos.width !=0 && pos.height !=0){
-            let data = video_ctx.getImageData(pos.left,pos.top-100,pos.width,pos.height) ;
-            temp_canvas_ctx.putImageData(data,0,0);
-            i.src = temp_canvas.toDataURL();
+            let data = video_ctx.getImageData(pos.left,pos.top-100,pos.width,pos.height)
+            temp_canvas_ctx.putImageData(data,0,0)
+            i.src = temp_canvas.toDataURL()
           }
-          
+
         }
       }
-      requestAnimationFrame(step);
+      requestAnimationFrame(step)
     }
-    requestAnimationFrame(step);
-  });
+    requestAnimationFrame(step)
+  })
 
 
 function new_video(){
